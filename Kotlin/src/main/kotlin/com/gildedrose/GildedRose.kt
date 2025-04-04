5a9d909dfd200d0490f50e72e8a832c523f139aa
Ollie Kennedy <ollie.kennedy1@gmail.com>
2025-04-04 15:15:43 +0100

    this always equates to 0

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -47,7 +47,7 @@ class GildedRose(val items: List<Item>) {
                             }
                         }
                     } else {
-                        item.quality = item.quality - item.quality
+                        item.quality = 0
                     }
                 } else {
                     if (item.quality < 50) {
