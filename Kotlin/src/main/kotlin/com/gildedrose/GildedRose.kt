b822638ca6f262b8dc1ef8d562e5b2332600c896
Ollie Kennedy <ollie.kennedy1@gmail.com>
2025-04-04 15:26:03 +0100

    flip if so name is checked first

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -27,8 +27,8 @@ class GildedRose(val items: List<Item>) {
                     }
                 }
             } else {
-                if (item.quality > 0) {
-                    if (item.name != SULFURAS) {
+                if (item.name != SULFURAS) {
+                    if (item.quality > 0) {
                         item.quality -= 1
                     }
                 }
